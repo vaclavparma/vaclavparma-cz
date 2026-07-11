1263576fd66bedbc3f33877c92fd1df05d90bdc1
Václav Parma <github@vaclavparma.cz>
2026-07-11 16:19:12 +0200

    Change cto to founding engineer

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -16,7 +16,7 @@ export const ui = {
     about_1b:
       " let a jsem student, programátor a maker z České republiky. Studuji na Fakultě informačních technologií ČVUT v Praze.",
     about_2:
-      "Pracuji jako CTO (Chief Technology Officer) ve společnosti Oscillator Labs s.r.o., kde mám možnost nejen využívat své dosavadní znalosti k řešení praktických problémů, ale také se neustále učit novým věcem.",
+      "Pracuji jako Founding Engineer ve společnosti Oscillator Labs s.r.o., kde mám možnost nejen využívat své dosavadní znalosti k řešení praktických problémů, ale také se neustále učit novým věcem.",
     about_3:
       "Většinu svých znalostí jsem získal metodou pokus / omyl při práci na vlastních projektech. Jedním z nich je i tato webová stránka. Baví mě vytvářet menší programy, které mi pomáhají objevovat nové technologie a zároveň prohlubují mé stávající dovednosti. Občas se také věnuji hardwaru, například při experimentech s Arduinem nebo při stavbě 8-bitového počítače.",
     contact_title: "Kontakt",
@@ -33,7 +33,7 @@ export const ui = {
     about_1b:
       " years old and I am a student, programmer and maker from Czech Republic.  I study at the Faculty of Information Technology at the Czech Technical University in Prague.",
     about_2:
-      "I work as CTO (Chief Technology Officer) at Oscillator Labs s.r.o., where I have the opportunity not only to use my existing knowledge to solve practical problems, but also to constantly learn new things.",
+      "I work as Founding Engineer at Oscillator Labs s.r.o., where I have the opportunity not only to use my existing knowledge to solve practical problems, but also to constantly learn new things.",
     about_3:
       "Most of my knowledge has been gained through trial / error while working on my own projects. One of them is this website. I enjoy creating smaller programs that help me discover new technologies while deepening my existing skills. I also occasionally work on hardware, such as experimenting with Arduino or building an 8-bit computer.",
     contact_title: "Contact",
